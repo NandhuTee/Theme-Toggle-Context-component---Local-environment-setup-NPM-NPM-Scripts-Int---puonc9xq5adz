2b94bd0318691196aafd8d5948ed10fe072871aa
Nandhini_T <rtnandhutee@gmail.com>
2024-03-29 16:00:43 +0530

    theme-toggle-context-first_update

--- pages/components/LocalThemedBox.js
+++ pages/components/LocalThemedBox.js
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { ThemeContext } from './ThemeProvider';
 
 const LocalThemedBox = () => {
+  const {theme, toggleTheme} = useContext(ThemeContext);
+  const [localTheme, setLocalTheme] = useState(theme);
+  console.log(theme);
+  console.log('localtheme', localTheme)
+  // useEffect(()=>{
+  //   setLocalTheme(theme)
+  // },[theme])
   return (
     <div
       style={{ width: '200px', height: '200px', border: '2px solid green' }}
       id='local-themed-box'
+      className={`bg-${localTheme === 'light'? 'light': 'dark'} txt-${localTheme === 'light'? 'light': 'dark'}`}
     >
-      {/* Write code below this line */}
+      hiiii
+      <button 
+        onClick={()=>setLocalTheme(localTheme === 'light'? 'dark': 'light')}
+        // onClick={toggleTheme}
+        className={`btn btn-${localTheme === 'light'? 'light': 'dark'}`}
+      >
+        Toggle local theme to {localTheme === 'light'? 'dark': 'light' }
+      </button>
     </div>
   );
 };
--- pages/components/Main.js
+++ pages/components/Main.js
@@ -1,16 +1,36 @@
 'use client';
-import React from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 import { LocalThemedBox } from './LocalThemedBox';
 import { ThemeContext } from './ThemeProvider';
 
 const Main = () => {
+  const {theme, toggleTheme} = useContext(ThemeContext);
+  const [tempState, setTempState] = useState(true);
+
+  function toggleThemeInternalFunc(){
+    toggleTheme()
+    const newValue = tempState ? false : true;
+    setTempState(newValue)
+  }
+
+  useEffect(()=>{
+    
+    setTempState(true);
+  },[tempState])
+
   return (
-    <div className={'container'} id='themed-page'>
+    <div className={`container bg-${theme === 'light'? 'light': 'dark'} txt-${theme === 'light'? 'light': 'dark'}`} 
+      id='themed-page'
+    >
       <p id='themed-text-container'>lorem ipsum dolor iterit n stuff</p>
-      <button className='btn' id='themed-button'>
+      <button 
+        className={`btn btn-${theme === 'light'? 'light': 'dark'}`} 
+        id='themed-button'
+        onClick={toggleThemeInternalFunc}
+      >
         Themed Button
       </button>
-      <LocalThemedBox />
+      {tempState && <LocalThemedBox />}
     </div>
   );
 };
